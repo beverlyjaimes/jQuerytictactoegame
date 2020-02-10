714a5c9387fa716f6e300f76bd38a0b89a93527d
Dev Bev <beverly.jaimespuente@gmail.com>
2020-02-09 21:35:46 -0600

    added x and o to game

--- tictactoe/js/mainFunction.js
+++ tictactoe/js/mainFunction.js
@@ -1,1 +1,16 @@
-var move = 1;
+let move = 1;
+let play = true;
+
+$('.square').click(function () {
+    if( $(this).text()== "" && play){
+        if((move %2) == 1){
+            $(this).append("X");
+            $(this).css('color', '#61892f');
+        }else {
+            $(this).append("O");
+            $(this).css('color', '#e85a4f');
+        }
+        move++;
+    }
+
+});
